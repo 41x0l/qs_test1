7aebb4c6be9640da3ac092a902cd7f58f4acd7f1
Ion Savin <98312503+l0x14@users.noreply.github.com>
2022-06-08 14:14:16 +0300

    Fix missing assignments.

--- src/contract.ts
+++ src/contract.ts
@@ -8,7 +8,7 @@ export function handleSync(event: Sync): void {
     let entity = new SyncEntity(event.transaction.from.toHex())
     entity.blockNumber = event.block.number;
     entity.index = event.transaction.index;
-    event.params.reserve0;
-    event.params.reserve1;
+    entity.reserve0 = event.params.reserve0;
+    entity.reserve1 = event.params.reserve1;
     entity.save();
 }
